4ba331f5cbe9bd2e416cd782f3892ff0c0ff2f9e
aaronmt1011 <aaronmt1011@gmail.com>
2022-12-15 22:48:41 -0700

    Add function that determines winner of a round

--- javascript.js
+++ javascript.js
@@ -5,18 +5,57 @@ function getComputerChoice() {
     let comChoice = Math.floor(Math.random() * (4 - 1) + 1);
     
     // if... else uses answer from above to pick between choices of rock, paper, scissors
-    if ( comChoice === 1 ) {
-        return "Rock";
-    } else if ( comChoice === 2) {
-        return "Paper";
+    if (comChoice === 1 ) {
+        return "rock";
+    } else if (comChoice === 2) {
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 // playRound() uses 2 parameters to determine the winner
 function playRound(playerSelection, computerSelection) {
-    //
-
-
+    // uses if... else to determine winner
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+        // paper beats rock, user loses
+            return "You Lose! Paper beats rock.";
+        } else if (computerSelection === "scissors") {
+        // rock beats scissors, user wins
+            return "You Won! Rock beats scissors.";
+        } else {
+        // rock ties with rock, tie
+            return "You tie! Rock ties with rock.";
+        }
+        
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+        // scissors beats paper, user loses
+            return "You Lose! Scissors beats paper.";
+        } else if (computerSelection === "rock") {
+        // paper beats rock, user wins
+            return "You Won! Paper beats rock.";
+        } else {
+        // paper ties with paper, tie
+            return "You tie! Paper ties with paper.";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+        // rock beats scissors, user loses
+            return "You Lose! Rock beats scissors.";
+        } else if (computerSelection === "paper") {
+        // scissors beats paper, user wins
+            return "You Won! Scissors beats paper.";
+        } else {
+        // scissors ties with scissors, tie
+            return "You tie! Scissors ties with scissors.";
+        }
+    }
 }
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
