eb01dc41b4a94e4df14fb70abe966a043af4df44
aaronmt1011 <aaronmt1011@gmail.com>
2022-12-15 23:43:00 -0700

    add game() function that is halfway complete.

--- javascript.js
+++ javascript.js
@@ -15,47 +15,83 @@ function getComputerChoice() {
 }
 
 // playRound() uses 2 parameters to determine the winner
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) { 
     // uses if... else to determine winner
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
         // paper beats rock, user loses
+            gameResult = "loser";
             return "You Lose! Paper beats rock.";
         } else if (computerSelection === "scissors") {
         // rock beats scissors, user wins
+            gameResult = "winner";
             return "You Won! Rock beats scissors.";
         } else {
         // rock ties with rock, tie
+            gameResult = "tie";
             return "You tie! Rock ties with rock.";
         }
-        
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
         // scissors beats paper, user loses
+            gameResult = "loser";
             return "You Lose! Scissors beats paper.";
         } else if (computerSelection === "rock") {
         // paper beats rock, user wins
+            gameResult = "winner";
             return "You Won! Paper beats rock.";
         } else {
         // paper ties with paper, tie
+            gameResult = "tie";
             return "You tie! Paper ties with paper.";
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
         // rock beats scissors, user loses
+            gameResult = "loser";
             return "You Lose! Rock beats scissors.";
-        } else if (computerSelection === "paper") {
+        } else if (computerSelectin === "paper") {
         // scissors beats paper, user wins
+            gameResult = "winner";
             return "You Won! Scissors beats paper.";
         } else {
         // scissors ties with scissors, tie
+            gameResult = "tie";
             return "You tie! Scissors ties with scissors.";
         }
     }
 }
 
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+// game() use for loop to cycle through 5 rounds before determining winner
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    
 
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i++) {
+        if (gameResult === "winner") {
+            playRound(playerSelection, computerSelection);
+            console.log(playRound(playerSelection, computerSelection));
+            playerScore ++;
+            console.log(playerScore);
+        } else if (gameResult === "loser") {
+            playRound(playerSelection, computerSelection);
+            console.log(playRound(playerSelection, computerSelection));
+            computerScore ++;
+            console.log(computerScore);
+        } else {
+            playRound(playerSelection, computerSelection);
+            console.log(playRound(playerSelection, computerSelection));
+            playerScore += 1;
+            computerScore += 1;
+        }
+    }
+}
+
+
+// uses windows prompt to let user pick their choice
+let playerPrompt = prompt("Please enter your choice between rock, paper, scissors");
+let playerSelection = playerPrompt.toLowerCase();
+let gameResult;
+const computerSelection = getComputerChoice();
